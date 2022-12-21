ee50db40ef75eb7254d8d8dbb4e501100f3ff9f3
Gustav <gustav_lowen@hotmail.com>
2022-12-21 14:56:43 +0100

    Remove axios from fetch

--- src/api/FetchQuestion.tsx
+++ src/api/FetchQuestion.tsx
@@ -1,5 +1,3 @@
-import axios from "axios"
-
 const baseURL = "https://the-trivia-api.com/api/questions?categories="
 
 export const fetchQuestions = async (
@@ -7,15 +5,11 @@ export const fetchQuestions = async (
   difficulty: string
 ): Promise<any> => {
   try {
-    const response = await axios.get(
-      `${baseURL}${category}&limit=1&difficulty=${difficulty}`
-    )
-    console.log(response.data)
-    return response.data
+    const response = await (
+      await fetch(`${baseURL}${category}&limit=1&difficulty=${difficulty}`)
+    ).json()
+    return response
   } catch (error) {
     throw new Error(`Error fetching the questions. ${error}`)
   }
 }
-
-// Full URL example
-// `https://the-trivia-api.com/api/questions?categories=arts_and_literature&limit=1&region=SE&difficulty=easy`
