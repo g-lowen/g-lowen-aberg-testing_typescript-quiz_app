420711bfbcba35da5098afbf9905af421ff4ba9c
Gustav <gustav_lowen@hotmail.com>
2022-12-21 15:00:41 +0100

    Add BDD test testing calculate score using Cucumber

--- src/__tests__/BDD/CalculateScoreBDD.tests.ts
+++ src/__tests__/BDD/CalculateScoreBDD.tests.ts
@@ -1,77 +1,66 @@
-import { loadFeature, defineFeature } from 'jest-cucumber';
-import { useCalculateScore } from '../../utilities/useCalculateScore';
-import { Points } from '../../interfaces/IPoints';
-const feature = loadFeature('./specs/features/calcScore.feature');
+import { loadFeature, defineFeature } from "jest-cucumber"
+import { useCalculateScore } from "../../utilities/useCalculateScore"
+import { Points } from "../../interfaces/IPoints"
+const feature = loadFeature("./specs/features/calcScore.feature")
 
- defineFeature(feature, (test) => {
-    let calcResult : number = 0;
-    let questionAnswers: Points[] = []
-    let questionAnswer: Points = {difficultyPoints: 0, timePoints: 0 }
+defineFeature(feature, (test) => {
+  let calcResult: number = 0
+  let questionAnswers: Points[] = []
+  let questionAnswer: Points = { difficultyPoints: 0, timePoints: 0 }
 
-    
-    test('Add two numbers', ({ given, when, then }) => {
-        given(/^x: (\d+), y: (\d+)$/, (diff, time) => {
-            questionAnswer.difficultyPoints = parseInt(diff)
-            questionAnswer.timePoints = parseInt(time)
-            questionAnswers.push(questionAnswer)
-            console.log(questionAnswers);
-        })
-        when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
-            questionAnswer.difficultyPoints = parseInt(diff)
-            questionAnswer.timePoints = parseInt(time)
-            questionAnswers.push(questionAnswer)
-        })
-        when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
-            questionAnswer.difficultyPoints = parseInt(diff)
-            questionAnswer.timePoints = parseInt(time)
-            questionAnswers.push(questionAnswer)
-            console.log(questionAnswers);
-        })
-        when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
-            questionAnswer.difficultyPoints = parseInt(diff)
-            questionAnswer.timePoints = parseInt(time)
-            questionAnswers.push(questionAnswer)
-            console.log(questionAnswers);
-        })
-        when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
-            questionAnswer.difficultyPoints = parseInt(diff)
-            questionAnswer.timePoints = parseInt(time)
-            questionAnswers.push(questionAnswer)
-            console.log(questionAnswers);
-        })
-        when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
-            questionAnswer.difficultyPoints = parseInt(diff)
-            questionAnswer.timePoints = parseInt(time)
-            questionAnswers.push(questionAnswer)
-            console.log(questionAnswers);
-        })
-        when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
-            questionAnswer.difficultyPoints = parseInt(diff)
-            questionAnswer.timePoints = parseInt(time)
-            questionAnswers.push(questionAnswer)
-            console.log(questionAnswers);
-        })
-        when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
-            questionAnswer.difficultyPoints = parseInt(diff)
-            questionAnswer.timePoints = parseInt(time)
-            questionAnswers.push(questionAnswer)
-            console.log(questionAnswers);
-        })
-        when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
-            questionAnswer.difficultyPoints = parseInt(diff)
-            questionAnswer.timePoints = parseInt(time)
-            questionAnswers.push(questionAnswer)
-            console.log(questionAnswers);
-            
-        })
+  test("Add two numbers", ({ given, when, then }) => {
+    given(/^x: (\d+), y: (\d+)$/, (diff, time) => {
+      questionAnswer.difficultyPoints = parseInt(diff)
+      questionAnswer.timePoints = parseInt(time)
+      questionAnswers.push(questionAnswer)
+    })
+    when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
+      questionAnswer.difficultyPoints = parseInt(diff)
+      questionAnswer.timePoints = parseInt(time)
+      questionAnswers.push(questionAnswer)
+    })
+    when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
+      questionAnswer.difficultyPoints = parseInt(diff)
+      questionAnswer.timePoints = parseInt(time)
+      questionAnswers.push(questionAnswer)
+    })
+    when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
+      questionAnswer.difficultyPoints = parseInt(diff)
+      questionAnswer.timePoints = parseInt(time)
+      questionAnswers.push(questionAnswer)
+    })
+    when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
+      questionAnswer.difficultyPoints = parseInt(diff)
+      questionAnswer.timePoints = parseInt(time)
+      questionAnswers.push(questionAnswer)
+    })
+    when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
+      questionAnswer.difficultyPoints = parseInt(diff)
+      questionAnswer.timePoints = parseInt(time)
+      questionAnswers.push(questionAnswer)
+    })
+    when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
+      questionAnswer.difficultyPoints = parseInt(diff)
+      questionAnswer.timePoints = parseInt(time)
+      questionAnswers.push(questionAnswer)
+    })
+    when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
+      questionAnswer.difficultyPoints = parseInt(diff)
+      questionAnswer.timePoints = parseInt(time)
+      questionAnswers.push(questionAnswer)
+    })
+    when(/^x: (\d+), y: (\d+)$/, (diff, time) => {
+      questionAnswer.difficultyPoints = parseInt(diff)
+      questionAnswer.timePoints = parseInt(time)
+      questionAnswers.push(questionAnswer)
+    })
 
-        when('Calling the function to add two numbers', () => {
-            calcResult = useCalculateScore(questionAnswers);
-            // expect(calcResult).toBe(calcResult);
-        });
+    when("Calling the function to add two numbers", () => {
+      calcResult = useCalculateScore(questionAnswers)
+    })
 
-        then(/^the result should be (\d+)$/, (expected) => {
-            expect(calcResult).toBe(parseInt(expected));
-        });
-  });
-});
+    then(/^the result should be (\d+)$/, (expected) => {
+      expect(calcResult).toBe(parseInt(expected))
+    })
+  })
+})
